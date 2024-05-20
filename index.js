498cd7476264774e30d038cca2cfe6178bc0daa0
ajamil121 <aimanjamil121@gmail.com>
2024-05-20 12:12:36 +0500

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const search=document.querySelector('.search-box button');
 const mainWeather=document.querySelector('.main-weather');
 const weatherDetails=document.querySelector('.weather-details');
 const error404= document.querySelector('.not-found');
-// const cityHide= document.querySelector('.city-hide');
+
 
 search.addEventListener('click', () => {
   const APIKey ='01ab9b409530a9e3a6140f8b22ab9cee';
@@ -12,17 +12,20 @@ search.addEventListener('click', () => {
   if(city == '')
     return;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    .then(response => response.json()).then(json => {
+
+        console.log(fetch)
 
         if(json.cod == '404'){
-            container.style.height ="400px";
+            container.style.height ='400px';
             mainWeather.classList.remove('active');
             weatherDetails.classList.remove('active');
             error404.classList.add('active');
             return;
         } 
         
-        container.style.height ="555px";
+        container.style.height ='555px';
         mainWeather.classList.add('active');
         weatherDetails.classList.add('active');
         error404.classList.remove('active');
@@ -39,32 +42,32 @@ search.addEventListener('click', () => {
             break;
              
             case 'Rain':
-                image.src ='./assest/rain.png';
+                image.src ='assest/rain.png';
             break;
             
             case 'Snow':
-                image.src ='./assest/snow.png';
+                image.src ='assest/snow.png';
             break;
             
             case 'Clouds':
-                image.src ='./assest/cloud.png';
+                image.src ='assest/cloud.png';
             break;
             case 'Mist':
-                image.src ='./assest/mist.png';
+                image.src ='assest/mist.png';
             break;
-            
+
              case 'Haze':
-             image.src ='./assest/mist.png';
+             image.src ='assest/mist.png';
          break;
 
             default:
-                image.src='./assest/cloud.png';
-
+                image.src='assest/cloud.png';
+            
         }
-        temperature.innerHTML= `${parseInt(json.main.temp)} <span>°C</span>`;
-        description.innerHTML = `${json.weather[0].description}`;
-        humidity.innerHTML = `${json.main.humidity}%`;
-        wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
+        temperature.innerHTML= `${parseInt(json.main.temp)} <span> °C </span>`;
+        // description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
     });
 
 });
